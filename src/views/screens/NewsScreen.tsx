251b738f87b4b268802a3d37aaf4cefdc416d296
Gabriel Lim <gabriellim07@gmail.com>
2024-06-30 14:55:21 +0800

    feat: add search news ui

--- src/views/screens/NewsScreen.tsx
+++ src/views/screens/NewsScreen.tsx
@@ -1,7 +1,31 @@
+import {Button, StyleSheet, TextInput, View} from 'react-native';
+
 const NewsScreen = () => {
-    return (
-        <></>
-    )
+  return (
+    <View style={styles.container}>
+      <View style={styles.searchContainer}>
+        <TextInput placeholder={'Search news..'} style={styles.input} />
+        <Button title={'Search'} />
+      </View>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  input: {
+    borderRadius: 4,
+    borderColor: 'gray',
+    borderWidth: 1,
+    padding: 12,
+    flex: 1,
+  },
+});
 
 export default NewsScreen;
