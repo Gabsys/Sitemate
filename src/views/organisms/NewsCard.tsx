df36a4b0a80bd360ec914074e7e043a2fe5958ba
Gabriel Lim <gabriellim07@gmail.com>
2024-06-30 17:01:52 +0800

    fix: fix wrong property used

--- src/views/organisms/NewsCard.tsx
+++ src/views/organisms/NewsCard.tsx
@@ -6,7 +6,7 @@ interface NewsCardProps {
 
 const NewsCard = (props: NewsCardProps) => {
   const {data} = props;
-  const {author, title, content, publishedAt, urlToImage} = data;
+  const {author, title, description, publishedAt, urlToImage} = data;
 
   return (
     <View style={styles.container}>
@@ -18,10 +18,10 @@ const NewsCard = (props: NewsCardProps) => {
         />
       )}
       <Text style={styles.title}>{title}</Text>
-      <Text>{content}</Text>
+      <Text>{description}</Text>
       <View style={styles.footerContainer}>
         <Text style={styles.footer}>Author: {author}</Text>
-        <Text style={styles.footer}>{publishedAt}</Text>
+        <Text style={styles.footer}>Published at: {publishedAt}</Text>
       </View>
     </View>
   );
